4d3ebb58a2fab3e267bb18e77b2da319fd844b0e
tougha sara <toughasarah@gmail.com>
2025-03-23 15:59:01 +0000

    modification de resume candidat terminer

--- project/resources/views/candidat/candidatresume.blade.php
+++ project/resources/views/candidat/candidatresume.blade.php
@@ -31,7 +31,7 @@
                 </div>
             </div>
             <div class="flex space-x-3">
-                <a href="{{ route('resume.update', $resume->id) }}" class="text-blue-800 hover:text-blue-600" title="Modifier">
+                <a href="{{ route('resume.edit', $resume->id) }}" class="text-blue-800 hover:text-blue-600" title="Modifier">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                     </svg>
